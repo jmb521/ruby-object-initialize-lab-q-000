690d3eb757d11a232640dec874bee23c95cd9c14
Jennifer Pazos <jmp521@gmail.com>
2016-05-03 11:00:11 +0000

    nitrous-backup at Tue May  3 11:00:11 UTC 2016

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,5 @@
 class Dog
-   def initialize(name, breed="Mut")
-     @name = name
-     @breed = breed
-
-
-   end
+  
   def name=(name)
     @name = name
   end
@@ -16,6 +11,9 @@ class Dog
   def breed=(breed)
     @breed = breed
 
+  end
+  def breed 
+    @breed
   end
 
 end
